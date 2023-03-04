ba85fbecab395f4b55fd172a98bcb3f489b33883
COKER BUSAYO OLADIPUPO <coker.busayo@gmail.com>
2023-03-04 00:14:59 +0000

    made modifications to the frontend

--- frontend-react-js/src/pages/NotificationsFeedPage.js
+++ frontend-react-js/src/pages/NotificationsFeedPage.js
@@ -11,74 +11,74 @@ import ReplyForm from '../components/ReplyForm';
 import Cookies from 'js-cookie'
 
 export default function NotificationsFeedPage() {
-const [activities, setActivities] = React.useState([]);
-const [popped, setPopped] = React.useState(false);
-const [poppedReply, setPoppedReply] = React.useState(false);
-const [replyActivity, setReplyActivity] = React.useState({});
-const [user, setUser] = React.useState(null);
-const dataFetchedRef = React.useRef(false);
+  const [activities, setActivities] = React.useState([]);
+  const [popped, setPopped] = React.useState(false);
+  const [poppedReply, setPoppedReply] = React.useState(false);
+  const [replyActivity, setReplyActivity] = React.useState({});
+  const [user, setUser] = React.useState(null);
+  const dataFetchedRef = React.useRef(false);
 
-const loadData = async () => {
+  const loadData = async () => {
     try {
-    const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/notifications`
-    const res = await fetch(backend_url, {
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/notifications`
+      const res = await fetch(backend_url, {
         method: "GET"
-    });
-    let resJson = await res.json();
-    if (res.status === 200) {
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
         setActivities(resJson)
-    } else {
+      } else {
         console.log(res)
-    }
+      }
     } catch (err) {
-    console.log(err);
+      console.log(err);
     }
-};
+  };
 
-const checkAuth = async () => {
+  const checkAuth = async () => {
     console.log('checkAuth')
     // [TODO] Authenication
     if (Cookies.get('user.logged_in')) {
-    setUser({
+      setUser({
         display_name: Cookies.get('user.name'),
         handle: Cookies.get('user.username')
-    })
+      })
     }
-};
+  };
 
-React.useEffect(()=>{
+  React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
 
     loadData();
     checkAuth();
-}, [])
+  }, [])
 
-return (
+  return (
     <article>
-    <DesktopNavigation user={user} active={'notifications'} setPopped={setPopped} />
-    <div className='content'>
+      <DesktopNavigation user={user} active={'notifications'} setPopped={setPopped} />
+      <div className='content'>
         <ActivityForm  
-        popped={popped}
-        setPopped={setPopped} 
-        setActivities={setActivities} 
+          popped={popped}
+          setPopped={setPopped} 
+          setActivities={setActivities} 
         />
         <ReplyForm 
-        activity={replyActivity} 
-        popped={poppedReply} 
-        setPopped={setPoppedReply} 
-        setActivities={setActivities} 
-        activities={activities} 
+          activity={replyActivity} 
+          popped={poppedReply} 
+          setPopped={setPoppedReply} 
+          setActivities={setActivities} 
+          activities={activities} 
         />
         <ActivityFeed 
-        title="Notifications" 
-        setReplyActivity={setReplyActivity} 
-        setPopped={setPoppedReply} 
-        activities={activities} 
+          title="Notifications" 
+          setReplyActivity={setReplyActivity} 
+          setPopped={setPoppedReply} 
+          activities={activities} 
         />
-    </div>
-    <DesktopSidebar user={user} />
+      </div>
+      <DesktopSidebar user={user} />
     </article>
-);
+  );
 }
